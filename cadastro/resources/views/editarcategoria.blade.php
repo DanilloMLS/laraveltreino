dee3dbca83012e4f5b540a06acbb2c08921cb7ce
DanilloMLS <moraesdanillo10@gmail.com>
2019-07-20 22:53:50 -0300

    Projeto cadastro de categorias com formulários

---
+++ cadastro/resources/views/editarcategoria.blade.php
@@ -0,0 +1,17 @@
+@extends('layout.app',["current"=>"categorias"])
+
+@section('body')
+    <div class="card border">
+        <div class="card-body">
+            <form action="/categorias/{{$cat->id}}" method="POST">
+                @csrf
+                <div class="form-group">
+                    <label for="nomeCategoria">Nome da Categoria</label>
+                    <input type="text" class="form-control" name="nomeCategoria" value="{{$cat->nome}}" id="nomeCategoria" placeholder="Categoria">
+                </div>
+                <button type="submit" class="btn btn-primary">Salvar</button>
+                <button type="cancel" class="btn btn-danger">Cancelar</button>
+            </form>
+        </div>
+    </div>
+@endsection
