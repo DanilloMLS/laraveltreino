f85a36700ad7d1d31f6e80c44f3535d56ffc23ae
DanilloMLS <moraesdanillo10@gmail.com>
2019-07-24 14:01:50 -0300

    Relacionamento Um-pra-um com Eloquent e uso do método hasOne()

---
+++ rel-um-pra-um/routes/web.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+use App\Cliente;
+use App\Endereco;
+
+Route::get('/clientes', function () {
+    $clientes = Cliente::all();
+    foreach ($clientes as $c ) {
+        echo "<p>ID: ". $c->id ."</p>";
+        echo "<p>Nome: ". $c->nome ."</p>";
+        echo "<p>Telefone: ". $c->telefone ."</p>";
+        //$e = Endereco::where('cliente_id',$c->id)->first();
+
+        //esses campos não fazem parte do cliente, mas é possível chamá-los
+        //com o hasOne em App\Cliente
+        echo "<p>Rua: ". $c->endereco->rua ."</p>";
+        echo "<p>Número: ". $c->endereco->numero ."</p>";
+        echo "<p>Bairro: ". $c->endereco->bairro ."</p>";
+        echo "<p>Cidade: ". $c->endereco->cidade ."</p>";
+        echo "<p>UF: ". $c->endereco->uf ."</p>";
+        echo "<p>CEP: ". $c->endereco->cep ."</p>";
+        echo "<hr>";
+    }
+});
+
+Route::get('/enderecos', function () {
+    $enderecos = Endereco::all();
+    foreach ($enderecos as $e ) {
+        echo "<p>Cliente: ". $e->cliente_id ."</p>";
+        echo "<p>Rua: ". $e->rua ."</p>";
+        echo "<p>Número: ". $e->numero ."</p>";
+        echo "<p>Bairro: ". $e->bairro ."</p>";
+        echo "<p>Cidade: ". $e->cidade ."</p>";
+        echo "<p>UF: ". $e->uf ."</p>";
+        echo "<p>CEP: ". $e->cep ."</p>";
+        echo "<hr>";
+    }
+});
