4baf7cdcf23612d2992453f3c75a32656b39aa28
DanilloMLS <moraesdanillo10@gmail.com>
2019-08-02 10:16:46 -0300

    Implementação de um login básico

--- middleware-login/routes/web.php
+++ middleware-login/routes/web.php
@@ -10,7 +10,44 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/produtos', 'ProdutoControlador@index');
+
+Route::get('/negado', function () {
+    return 'Acesso negado.';
+})->name('negado');
+
+Route::post('/login', function (Request $req) {
+    $login_ok = false;
+    switch ($req->input('user')) {
+        case 'joao':
+            $login_ok = $req->input('passwd') === "senhajoao";
+            break;
+        case 'marcos':
+            $login_ok = $req->input('passwd') === "senhamarcos";
+            break;
+        default:
+        $login_ok = false;
+            break;
+    }
+
+    if ($login_ok) {
+        $login = ['user' => $req->input('user')];
+        $req->session()->put('login', $login);
+        return response("Login OK", 200);
+    } else {
+        $req->session()->flush();
+        return response("Erro no Login", 404);
+    }
+    
+});
+
+Route::get('/logout', function (Request $request) {
+    $request->session()->flush();
+    return response('Deslogado com Sucesso', 200);
 });
