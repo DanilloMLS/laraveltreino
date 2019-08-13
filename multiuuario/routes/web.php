3416b25e592327c274a8159f73a33a74ea38a95d
DanilloMLS <moraesdanillo10@gmail.com>
2019-08-13 10:21:14 -0300

    Criação da autenticação do Administrador

--- multiuuario/routes/web.php
+++ multiuuario/routes/web.php
@@ -18,5 +18,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-
-Route::get('/admin', 'AdminController@index')->name('homeadmin');
+Route::get('/admin', 'AdminController@index')->name('admin.dashboard');
+Route::get('/admin/login', 'Auth\AdminLoginController@index')->name('admin.login');
+Route::post('/admin/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
